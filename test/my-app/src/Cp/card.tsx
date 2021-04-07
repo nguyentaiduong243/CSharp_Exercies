b8343bcf740562ca7543aef6b2d33bfe3a82cce9
nguyentaiduong243@gmail.com <nguyentaiduong243@gmail.com>
2021-04-07 15:37:27 +0700

    first commit

---
+++ test/my-app/src/Cp/card.tsx
@@ -0,0 +1,21 @@
+export const Card = () => {
+    return (
+        <div>
+            <div>
+                <i className="fas fa-car" style={{fontSize: '100px'}}></i>
+            </div>
+        </div>
+    )
+}
+
+export const Card1 = () => {
+    return (
+        <div>
+            <div>
+                <i className="fas fa-car"></i>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
